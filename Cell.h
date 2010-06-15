18e253c4faf593cf73e2405af4e43015a5fa5a43
ccs4ever <ccs4ever@0dfad899-2ec3-e410-9402-18846525766d>
2010-06-15 05:24:19 +0000

    QTified the STL map/string in Cell, and merged it with zzcell, now deceased.
    I forgot that most of the mainwindow stuff was not
    autogenerated, only the ui_mainwindow.h was, so 
    I put that code back.
    
    It turns out a chunk of test code was in there, so
    it shouldn't be too long before I have hooked that
    back up.

--- Cell.h
+++ Cell.h
@@ -1,42 +1,55 @@
-/* Cell.h - headers for the zz cell
+/* dscell.h - headers for the zz cell
  * (c) 2010 John Ohno
  * Licensed under the GNU GPL v.3
  * Part of the DimScape project
  */
 
-#ifndef CELL_H
-#define CELL_H
+#ifndef ZZCELL_H
+#define ZZCELL_H
 
-using namespace std;
-#include <map>
-#include <string>
+#include <QHash>
+#include <QString>
 
-#define cellID int /* might change this later to int64_t or something */
+#define cellID qint64
 
-class Cell {
+
+class ZZCell : public QObject {
+	Q_OBJECT
+	
 	public:
-	Cell();
-	void setContent(char* cont);
-	char* getContent();
-	void setPos(cellID pos, string dimension);
-	cellID getPos(string dimension);
-	void setNeg(cellID neg, string dimension);
-	cellID getNeg(string dimension);
-	void setType(int type);
+	ZZCell();
+	QString getContent();
+	cellID getPos(QString& dimension);
+	cellID getNeg(QString& dimension);
 	int getType();
 	cellID getID();
 	void setID(cellID myId);
+	
+	public slots:
+	void setContent(QString& cont);
+	void setPos(cellID pos, QString& dimension);
+	void setNeg(cellID neg, QString& dimension);
+	void setType(int type);
+
+	signals:
+
+	void contentChanged(QString& content);
+	void poswardChanged(cellID pos, QString& dimension);
+	void negwardChanged(cellID neg, QString& dimension);
+	void typeChanged(int type);
+
 	protected:
-	string content;
-	map<string,cellID> posward;
-	map<string,cellID> negward;
+	QString content;
+	QHash<QString,cellID> posward;
+	QHash<QString,cellID> negward;
 	int type;
 	cellID id;
+
 };
 
 #ifndef ZZWORLD
 #define ZZWORLD
-map<cellID,Cell> world; /* this should include all cells */
+QHash<cellID,Cell> world; /* this should include all cells */
 #endif
 
 #endif
